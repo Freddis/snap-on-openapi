a585c5e4eaa7291e97966f2f3e40cecaf0a02173
Alex Sarychev <freddis336@gmail.com>
2025-07-15 13:59:23 +0300

    Adding shape description overrides for validators

--- services/ValidationUtils/ValidationUtils.ts
+++ services/ValidationUtils/ValidationUtils.ts
@@ -1,5 +1,5 @@
 import 'zod-openapi/extend';
-import z, {ZodRawShape, ZodType} from 'zod';
+import {array, number, object, ZodObject, ZodRawShape, ZodType} from 'zod';
 import {stringDateTransformer} from './transformers/stringDateTransformer';
 import {stringNumberTransformer} from './transformers/stringNumberTransfromer';
 
@@ -9,27 +9,36 @@ export class ValidationUtils {
     number: stringNumberTransformer,
   };
 
+  describeShape<TShape extends ZodRawShape>(
+    val: ZodObject<TShape>,
+    descriptions: Record<keyof TShape, string>
+  ): ZodObject<TShape> {
+    const newShape: ZodRawShape = {};
+    for (const entry of Object.entries(val.shape)) {
+      newShape[entry[0]] = entry[1].openapi({description: descriptions[entry[0]]});
+    }
+    return object(newShape) as ZodObject<TShape>;
+  }
+
   paginatedQuery<X extends ZodRawShape>(filter: X) {
-    return z
-          .object({
-            page: z.number().min(1).optional().default(1).openapi({description: 'Page number'}),
-            pageSize: z.number().min(1).optional().openapi({
-              description: 'Number of items to display in the page.',
-            }),
-          })
-          .extend(filter)
-          .openapi({description: 'Pagination parameters'});
+    return object({
+      page: number().min(1).optional().default(1).openapi({description: 'Page number'}),
+      pageSize: number().min(1).optional().openapi({
+        description: 'Number of items to display in the page.',
+      }),
+    }).extend(
+      filter
+    ).openapi({description: 'Pagination parameters'});
   }
 
   paginatedResponse<T extends ZodType>(arr: T) {
-    return z.object({
-      items: z.array(arr).openapi({description: 'Page or items'}),
-      info: z
-          .object({
-            count: z.number().openapi({description: 'Total number of items'}),
-            page: z.number().openapi({description: 'Current page'}),
-            pageSize: z.number().openapi({description: 'Number of itemss per page'}),
-          })
+    return object({
+      items: array(arr).openapi({description: 'Page or items'}),
+      info: object({
+        count: number().openapi({description: 'Total number of items'}),
+        page: number().openapi({description: 'Current page'}),
+        pageSize: number().openapi({description: 'Number of itemss per page'}),
+      })
           .openapi({description: 'Pagination details'}),
     });
   }
